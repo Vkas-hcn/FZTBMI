bec7a79b99b3d4ab2ef3033ed9694af45c983226
vkas <vkas@qq.com>
2025-03-18 15:27:54 +0800

    feat(dliGuso): 为数据类添加 @Keep 注解为 DliBean.kt 文件中的所有数据类添加 @Keep 注解，以防止在代码混淆时这些类被错误地修改或删除。这确保了应用程序的功能不会因代码混淆而受到影响。

--- dliGuso/src/main/java/com/ling/ding/sighed/txtbean/DliBean.kt
+++ dliGuso/src/main/java/com/ling/ding/sighed/txtbean/DliBean.kt
@@ -9,85 +9,103 @@ data class DliBean(
     val userManagement: UserManagement
 )
 
+@Keep
 data class AdOperations(
     val constraints: Constraints,
     val scheduling: Scheduling
 )
 
+@Keep
 data class AssetConfig(
     val identifiers: Identifiers
 )
 
+@Keep
 data class UserManagement(
     val profile: Profile
 )
 
+@Keep
 data class Constraints(
     val impressions: Impressions,
     val interactions: Interactions
 )
 
+@Keep
 data class Scheduling(
     val detection: Detection,
     val display: Display
 )
 
+@Keep
 data class Impressions(
     val daily: Int,
     val hourly: Int
 )
 
+@Keep
 data class Interactions(
     val clicks: Clicks,
     val errorHandling: ErrorHandling
 )
 
+@Keep
 data class Clicks(
     val daily: Int
 )
 
+@Keep
 data class ErrorHandling(
     val maxErrors: Int
 )
 
+@Keep
 data class Detection(
     val initialDelaySec: Int,
     val intervalSec: Int
 )
 
+@Keep
 data class Display(
     val delaySettings: DelaySettings,
     val frequencySec: Int
 )
 
+@Keep
 data class DelaySettings(
     val randomDelayMs: RandomDelayMs
 )
 
+@Keep
 data class RandomDelayMs(
     val max: Int,
     val min: Int
 )
 
+@Keep
 data class Identifiers(
     val campaignId: String,
     val facebook: Facebook,
     val linkData:String
 )
 
+@Keep
 data class Facebook(
     val placementId: String
 )
 
+@Keep
 data class Profile(
     val classification: String,
     val privileges: Privileges
 )
 
+@Keep
 data class Privileges(
     val upload: Upload
 )
 
+@Keep
 data class Upload(
     val enabled: Int
 )
